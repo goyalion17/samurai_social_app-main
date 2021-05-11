5df4f3be8f50874443b9d67d095c252efdc3d3f6
Irina Saveleva <goyalion17@gmail.com>
2021-05-11 15:46:39 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
+//
 const App = (props) => {
   return (
     <BrowserRouter>
