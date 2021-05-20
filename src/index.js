c61a849b9af6af873d94d516dfd7472703669a82
Irina Saveleva <goyalion17@gmail.com>
2021-05-20 17:55:37 +0300

    45. react-redux, connect, mapStateToProps, mapDispatchToProps

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/redux-store";
-import { Provider } from "./StoreContext";
+import { Provider } from "react-redux";
 
 let rerenderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
-      <Provider value={store}>
+      <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>,
@@ -24,7 +24,4 @@ store.subscribe(() => {
   rerenderEntireTree(state);
 });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
